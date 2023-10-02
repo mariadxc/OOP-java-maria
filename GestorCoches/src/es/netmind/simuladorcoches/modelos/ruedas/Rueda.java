ee781bb782a75a1d9a6d97c041fb00783491a032
mariadxc <maria-montserrat.martinez-arcas@dxc.com>
2023-10-02 11:55:02 +0200

    nueva funcionalidad Rueda

---
+++ GestorCoches/src/es/netmind/simuladorcoches/modelos/ruedas/Rueda.java
@@ -0,0 +1,10 @@
+package es.netmind.simuladorcoches.modelos.ruedas;
+
+public class Rueda {
+    private Long diametro;
+
+    public Rueda(Long diametro) {
+        this.diametro = diametro;
+    }
+
+}
