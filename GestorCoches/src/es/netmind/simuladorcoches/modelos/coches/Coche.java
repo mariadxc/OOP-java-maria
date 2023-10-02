aeb5795ac52dd47b1ad12263d6ccf311b67eacff
mariadxc <maria-montserrat.martinez-arcas@dxc.com>
2023-10-02 11:19:07 +0200

    añadido mensaje

--- GestorCoches/src/es/netmind/simuladorcoches/modelos/coches/Coche.java
+++ GestorCoches/src/es/netmind/simuladorcoches/modelos/coches/Coche.java
@@ -20,6 +20,7 @@ public abstract class Coche {
     }
 
     public boolean arrancar() {
+        //necesita un motor vcon valor
         motor.arrancar();
         this.arrancado = true;
         return true;
